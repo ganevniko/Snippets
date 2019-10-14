56152f4362cc87cab819a745e5e65ddfcf322f48
Niko Ganev <40749238+ganevniko@users.noreply.github.com>
2019-10-14 10:07:04 -0400

    Create sending-emails-with-python.py

---
+++ sending-emails-with-python.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+
+user=''
+password=''
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(user, password)
+
+msg = MIMEText("Testing the app")
+
+me = "sender@domain.com"
+you = "receiver@domain.com
+
+msg['Subject'] = 'Testing the app'
+msg['From'] = me
+msg['To'] = you
+
+server.sendmail(me, [you], msg.as_string())
