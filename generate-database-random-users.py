a7a43b265718a039d3f8100728c6ded75c1c36da
Niko Ganev <40749238+ganevniko@users.noreply.github.com>
2019-10-21 19:40:10 -0400

    Update generate-database-random-users.py

--- generate-database-random-users.py
+++ generate-database-random-users.py
@@ -29,3 +29,31 @@ df['Phone']=phone
 df['Email']=email
 
 df
+
+#All attributes available 
+'''get_cell()
+get_city()
+get_dob()
+get_email()
+get_first_name()
+get_full_name()
+get_gender()
+get_id()
+get_id_number()
+get_id_type()
+get_info()
+get_last_name()
+get_login_md5()
+get_login_salt()
+get_login_sha1()
+get_login_sha256()
+get_nat()
+get_password()
+get_phone()
+get_picture()
+get_postcode()
+get_registered()
+get_state()
+get_street()
+get_username()
+get_zipcode()'''
