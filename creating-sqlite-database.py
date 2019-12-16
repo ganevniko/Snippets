14558b1c3029381a1b44ce7af48ca3332e0356e9
Niko Ganev <40749238+ganevniko@users.noreply.github.com>
2019-12-16 12:45:55 -0500

    Update creating-sqlite-database.py

--- creating-sqlite-database.py
+++ creating-sqlite-database.py
@@ -1,7 +1,17 @@
 import sqlite3
+import pandas as pd
+
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 create_table = "CREATE TABLE users (id int, username text, password text)"
-user = (1, 'jose','asdf')
+cursor.execute(create_table)
+
+users = [(1, 'jose','asdf'),
+    (2,'rolf', 'asdf'),
+    (3,'anne','xyz')
+]
 insert_query = " INSERT INTO users VALUES (?,?,?)"
-cursor.execute(insert_query,user)
+cursor.executemany(insert_query,users)
+
+select_query="SELECT * FROM users"
+pd.DataFrame(cursor.execute(select_query))
