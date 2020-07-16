64882c317edad3b46f8bdb3e33ddb8ca45424b51
Niko Ganev <40749238+ganevniko@users.noreply.github.com>
2020-07-16 19:50:53 -0400

    Intro to c++

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+
+int main() {
+
+    double radius, area;
+
+
+    cout<<"Please enter the radius of your circle: "<<endl;
+    cin>>radius;
+
+    area = M_PI*radius*radius;
+
+    cout<<" The area of your circle with radius of "<< radius<<" is "<<area<<"."<<endl;
+
+    cout<< "ab\nc";
+
+    return 0;
+
+
+}
